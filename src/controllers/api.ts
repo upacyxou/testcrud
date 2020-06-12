a7d55b13d9ea4dcb6b46cc012aeb881ef0db755b
upacyxou <upacyxou@tutanota.com>
2020-06-13 00:53:25 +0400

    normal async/await and try/catch

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,42 +1,52 @@
-import { Context } from "koa";
-import { Controller, Post, Get, Delete, Put } from "koa-router-ts";
-import { UserMessagesModel } from "../models/api";
+import { Context } from 'koa'
+import { Controller, Post, Get, Delete, Put } from 'koa-router-ts'
+import { UserMessagesModel } from '../models/api'
 
-const db = UserMessagesModel;
+const db = UserMessagesModel
 
-@Controller("/api")
+@Controller('/api')
 export default class {
-  @Get("/getMessage")
+  @Get('/getMessage')
   async getMessage(ctx: Context) {
-    await db.find({}, (err, data) => {
-      if (err) console.log("Произошла ошибка при загрузке соообщений");
-      ctx.body = data;
-    });
+    try {
+      const allMessages = await db.find({})
+      ctx.body = allMessages
+    } catch (err) {
+      ctx.throw(400, err.message)
+    }
   }
 
-  @Post("/sendMessage")
+  @Post('/sendMessage')
   async sendMessage(ctx: Context) {
-    await db.insertMany(ctx.request.body, (err, data) => {
-      if (err) console.log("Произошла ошибка при отправке");
-    });
+    try {
+      const sendedMessage = await db.insertMany(ctx.request.body)
+      ctx.body = sendedMessage
+    } catch (err) {
+      ctx.throw(400, err.message)
+    }
   }
 
-  @Delete("/deleteMessage")
+  @Delete('/deleteMessage')
   async deleteMessage(ctx: Context) {
-    await db.findByIdAndRemove(ctx.request.body, (err, data) => {
-      if (err || !data) console.log("Произошла ошибка при удалении");
-    });
+    try {
+      const deletedMessage = await db.findByIdAndRemove(ctx.request.body)
+      ctx.body = deletedMessage
+    } catch (err) {
+      ctx.throw(400, err.message)
+    }
   }
 
-  @Put("/editMessage")
+  @Put('/editMessage')
   async editMessage(ctx: Context) {
-    await db.findByIdAndUpdate(
-      ctx.request.body._id,
-      { text: ctx.request.body.text },
-      { new: true },
-      (err, data) => {
-        if (err) console.log("Произошла ошибка при обновлении сообшения");
-      }
-    );
+    try {
+      const editedMessage = await db.findByIdAndUpdate(ctx.request.body._id, {
+        text: ctx.request.body.text,
+      })
+      if (!editedMessage)
+        ctx.throw(404, "Can't find a message. Please, try again.")
+      ctx.body = editedMessage
+    } catch (err) {
+      ctx.throw(400, err.message)
+    }
   }
 }
